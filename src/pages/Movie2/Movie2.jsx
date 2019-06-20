c2c75d47a52c33f0537e5d6e589e5e5649e52ba8
LeiDeMing <136371773@qq.com>
2019-06-20 13:51:28 +0800

    fix

--- src/pages/Movie2/Movie2.jsx
+++ src/pages/Movie2/Movie2.jsx
@@ -19,11 +19,11 @@ export default class Movie extends Component {
     }
 
     componentDidMount() {
-        this.dataMask=document.querySelector('#dataMask')
+        this.dataMask = document.querySelector('#dataMask')
     }
 
     onImportExcelNifty = file => {
-        this.dataMask.style.display="block"
+        this.dataMask.style.display = "block"
         this.fileCore(file, '样例号', (smapleArr) => {
             this.setState({
                 sampleNifty: smapleArr
@@ -32,7 +32,7 @@ export default class Movie extends Component {
     }
 
     onImportExcelLims = file => {
-        this.dataMask.style.display="block"
+        this.dataMask.style.display = "block"
         this.fileCore(file, 'Sample id', (smapleArr) => {
             this.setState({
                 sampleLims: smapleArr
@@ -67,25 +67,26 @@ export default class Movie extends Component {
                     sampleArr = data
                 }
                 callback && callback(sampleArr)
-                this.dataMask.style.display="none"
+                this.dataMask.style.display = "none"
             } catch (e) {
                 alert('文件类型不正确');
-                this.dataMask.style.display="none"
+                this.dataMask.style.display = "none"
                 return;
             }
         };
     }
 
     toNiftyXlms = () => {
-        this.dataMask.style.display="block"
+        this.dataMask.style.display = "block"
         const { sampleNifty, sampleLims } = this.state;
         let tempArr = []
         if (!sampleNifty || !sampleLims) {
             alert('数据为空')
+            this.dataMask.style.display = "none"
             return
         }
         console.log(sampleNifty, sampleLims)
-        for (let x = sampleNifty.length-1; x >= 0; x--) {
+        for (let x = sampleNifty.length - 1; x >= 0; x--) {
             for (let y = 0, yLen = sampleLims.length; y < yLen; y++) {
                 if (sampleNifty[x]['样例号'] === sampleLims[y]['sampleNum'] && !sampleLims[y]['isPush']) {
                     // sampleLims[y]['zcatlo'] = sampleNifty[x]['样本号']
@@ -100,7 +101,7 @@ export default class Movie extends Component {
                     //         tempArr.push(sampleLims[y])
                     //     }
                     // })
-                    sampleLims[y]['isPush']=true
+                    sampleLims[y]['isPush'] = true
                     tempArr.push(sampleLims[y])
                     break;
                 }
@@ -112,16 +113,16 @@ export default class Movie extends Component {
     downloadExl = (data, type) => {
         let option = {};
         option.fileName = 'excel'
-        let pureSample=[]
-        let pureData=[]
-        data.forEach(item=>{
-            if(pureSample.indexOf(item['Sample id'])===-1){
+        let pureSample = []
+        let pureData = []
+        data.forEach(item => {
+            if (pureSample.indexOf(item['Sample id']) === -1) {
                 pureSample.push(item['Sample id'])
             }
         })
-        pureSample.forEach(item=>{
-            for(let x=0;x<data.length;x++){
-                if(item===data[x]['Sample id']){
+        pureSample.forEach(item => {
+            for (let x = 0; x < data.length; x++) {
+                if (item === data[x]['Sample id']) {
                     pureData.push(data[x])
                     break;
                 }
@@ -131,13 +132,13 @@ export default class Movie extends Component {
             {
                 sheetData: pureData,
                 sheetName: 'sheet',
-                sheetFilter: ['样例号','任务单号', '任务单行项目号','zcatlo','','Sample id','技术路线ID','是否合格','QC','UR','GC','胎儿性别','胎儿浓度','T-score(chr21)','T-score(chr13)','风险指数(chr18)','Z-score(chr21)','Z-score(chr18)','Z-score(chr13)','Test(chr21)','Test(chr18)','Test(chr13)','Test(性染色体)','Test(常染色体)','Test区带(重复/缺失)','Test位点(重复/缺失)','Note1','Note2','Y%','疾病名称','dup/del','位置','大小'],
-                sheetHeader: ['样例号','任务单号', '任务单行项目号','zcatlo','','Sample id','技术路线ID','是否合格','QC','UR','GC','胎儿性别','胎儿浓度','T-score(chr21)','T-score(chr13)','风险指数(chr18)','Z-score(chr21)','Z-score(chr18)','Z-score(chr13)','Test(chr21)','Test(chr18)','Test(chr13)','Test(性染色体)','Test(常染色体)','Test区带(重复/缺失)','Test位点(重复/缺失)','Note1','Note2','Y%','疾病名称','dup/del','位置','大小']
+                sheetFilter: ['样例号', '任务单号', '任务单行项目号', 'zcatlo', '', 'Sample id', '技术路线ID', '是否合格', 'QC', 'UR', 'GC', '胎儿性别', '胎儿浓度', 'T-score(chr21)', 'T-score(chr13)', '风险指数(chr18)', 'Z-score(chr21)', 'Z-score(chr18)', 'Z-score(chr13)', 'Test(chr21)', 'Test(chr18)', 'Test(chr13)', 'Test(性染色体)', 'Test(常染色体)', 'Test区带(重复/缺失)', 'Test位点(重复/缺失)', 'Note1', 'Note2', 'Y%', '疾病名称', 'dup/del', '位置', '大小'],
+                sheetHeader: ['样例号', '任务单号', '任务单行项目号', 'zcatlo', '', 'Sample id', '技术路线ID', '是否合格', 'QC', 'UR', 'GC', '胎儿性别', '胎儿浓度', 'T-score(chr21)', 'T-score(chr13)', '风险指数(chr18)', 'Z-score(chr21)', 'Z-score(chr18)', 'Z-score(chr13)', 'Test(chr21)', 'Test(chr18)', 'Test(chr13)', 'Test(性染色体)', 'Test(常染色体)', 'Test区带(重复/缺失)', 'Test位点(重复/缺失)', 'Note1', 'Note2', 'Y%', '疾病名称', 'dup/del', '位置', '大小']
             }
         ];
 
         var toExcel = new ExportJsonExcel(option); //new
-        this.dataMask.style.display="none"
+        this.dataMask.style.display = "none"
         toExcel.saveExcel(); //保存
     }
 
@@ -153,9 +154,12 @@ export default class Movie extends Component {
                     <label htmlFor=""><input type='file' accept='.xlsx, .xls' onChange={this.onImportExcelNifty} />Nifty</label>
                 </li>
                 <li style={{ textAlign: "center", marginTop: "50px", listStyle: "none" }}>
-                    <Button onClick={this.toNiftyXlms} type="primary">生成XLMS</Button>
+                    <Button onClick={this.toNiftyXlms} type="primary">生成核对XLMS文件</Button>
                 </li>
-                <li id="dataMask" style={{display:'none',position:"fixed",left:'0',top:'0',width:'100%',height:'100%',background:'rgba(0,0,0,0.65)',color:'#ffffff',textAlign:'center',paddingTop:'260px'}}>数据加载中...</li>
+                <li id="dataMask" style={{ display: 'none', position: "fixed", left: '0', top: '0', width: '100%', height: '100%', background: 'rgba(0,0,0,0.65)', color: '#ffffff', textAlign: 'center', paddingTop: '260px' }}>数据加载中...</li>
+                <li style={{ textAlign: "center", listStyle: "none" }}>
+                    注：目前单个框只支持一个xlms文件，xlms文件只能有一个表有数据，且需要对应系统下载的原始表，请不要做修改！
+                </li>
             </ul>
         )
     }
