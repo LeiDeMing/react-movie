cb6b6194fc95af10477104b3a41bc442b2a710d2
LeiDeMing <136371773@qq.com>
2020-04-11 22:03:18 +0800

    add

--- src/layouts/ReadingLayout/components/Header/Header.jsx
+++ src/layouts/ReadingLayout/components/Header/Header.jsx
@@ -1,12 +1,63 @@
 import React, { PureComponent } from 'react'
-import '@/common/scss/basic.scss'
+import Table from 'rc-table';
 import { Menu, Icon } from 'antd';
+import '@/common/scss/basic.scss'
 const { SubMenu } = Menu;
 export default class Header extends PureComponent {
     constructor(props) {
         super(props)
         this.state = {
-            
+            columns: [
+                {
+                    title: 'Url',
+                    dataIndex: 'url',
+                    key: 'url',
+                    width: 200,
+                },
+                {
+                    title: 'Title',
+                    dataIndex: 'title',
+                    key: 'title',
+                    width: 200,
+                },
+                {
+                    title: 'Html_url',
+                    dataIndex: 'html_url',
+                    key: 'html_url',
+                    width: 200,
+                },
+                {
+                    title: 'Number',
+                    dataIndex: 'number',
+                    key: 'number',
+                    width: 60,
+                },
+                {
+                    title: 'Body',
+                    dataIndex: 'body',
+                    key: 'body',
+                    width: 200,
+                },
+                {
+                    title: 'Payload_all',
+                    dataIndex: 'payload_all',
+                    key: 'payload_all',
+                    width: 200,
+                },
+                {
+                    title: 'CreatedAt',
+                    dataIndex: 'createdAt',
+                    key: 'createdAt',
+                    width: 100,
+                },
+                {
+                    title: 'PpdatedAt',
+                    dataIndex: 'updatedAt',
+                    key: 'updatedAt',
+                    width: 100,
+                },
+            ],
+            data: []
         }
     }
 
@@ -14,8 +65,10 @@ export default class Header extends PureComponent {
     }
 
     render() {
+        const { state: { columns, data } } = this
         return (
             <div>
+                <Table columns={columns} data={data} />
             </div>
         );
     }
